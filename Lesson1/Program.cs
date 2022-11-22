dc025007d598722372a97c7e59a676100c8b2446
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-22 15:40:11 +0400

    Something like Sierpiński triangle in C-Sharp

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int xa = 0, ya = 0,
+    xb = 0, yb = 30,
+    xc = 60, yc = 30;
+
+int x = xa, y = ya;
+int count = 0;
+
+while (count < 2000) {
+    int what = new Random().Next(0, 3);
+    if (what == 0) {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1) {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2) {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
+Console.SetCursorPosition(0, 30);
