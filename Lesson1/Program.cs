eed69d90b1f14dbe986d3f164721eddd87459cdf
Maksim Aleksanin <maksim.g00gle@gmail.com>
2022-11-22 15:53:19 +0400

    add comments to code

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,13 +1,17 @@
-﻿Console.Clear();
+﻿Console.Clear(); 
+
+// set shape(triangle)
 int xa = 0, ya = 0,
     xb = 0, yb = 30,
     xc = 60, yc = 30;
 
-int x = xa, y = ya;
+int x = xa, y = ya;// set start point
 int count = 0;
 
 while (count < 2000) {
     int what = new Random().Next(0, 3);
+
+    // depending on the random number, set the coordinates to the middle of the initial values
     if (what == 0) {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
@@ -24,4 +28,4 @@ while (count < 2000) {
     Console.WriteLine("+");
     count++;
 }
-Console.SetCursorPosition(0, 30);
+Console.SetCursorPosition(0, 30);// set cursor to the end of shape
