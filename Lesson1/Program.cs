4e13906a842d3b7e9077cd906769b90127ca9b24
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-11-23 13:14:37 +0400

    chore: changes for new commit, set right triangle to isosceles triangle

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,16 +1,15 @@
 ﻿Console.Clear(); 
 
-// set shape(square)
-int xa = 0, ya = 0,
-    xb = 0, yb = 60,
-    xc = 60, yc = 0,
-    xd = 60, yd = 60;
+// set shape(triangle)
+int xa = 30, ya = 0,
+    xb = 0, yb = 30,
+    xc = 60, yc = 30;
 
 int x = xa, y = ya;// set start point
 int count = 0;
 
 while (count < 2000) {
-    int what = new Random().Next(0, 4);
+    int what = new Random().Next(0, 3);
 
     // depending on the random number, set the coordinates to the middle of the initial values
     if (what == 0) {
@@ -25,14 +24,8 @@ while (count < 2000) {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
     }
-    if (what == 3) {
-        x = (x + xd) / 2;
-        y = (y + yd) / 2;
-    }
-
     Console.SetCursorPosition(x, y);
     Console.Write("+");
     count++;
 }
-
 Console.SetCursorPosition(0, 30);// set cursor to the end of shape
