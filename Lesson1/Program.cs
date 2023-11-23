76bba1fc4e398a903735daf7d50a1cc4f98fc44b
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-11-23 13:19:15 +0400

    Revert "add comments to code, first revert, homework2, git-advanced"
    This reverts commit eed69d90b1f14dbe986d3f164721eddd87459cdf.

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,17 +1,13 @@
-﻿Console.Clear(); 
-
-// set shape(triangle)
-int xa = 30, ya = 0,
+﻿Console.Clear();
+int xa = 0, ya = 0,
     xb = 0, yb = 30,
     xc = 60, yc = 30;
 
-int x = xa, y = ya;// set start point
+int x = xa, y = ya;
 int count = 0;
 
 while (count < 2000) {
     int what = new Random().Next(0, 3);
-
-    // depending on the random number, set the coordinates to the middle of the initial values
     if (what == 0) {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
@@ -28,4 +24,4 @@ while (count < 2000) {
     Console.Write("+");
     count++;
 }
-Console.SetCursorPosition(0, 30);// set cursor to the end of shape
+Console.SetCursorPosition(0, 30);
